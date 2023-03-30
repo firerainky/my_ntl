97256080dbe15ac856e38735d257d2d732354848
zky <zhengkanyan@gmail.com>
2023-03-30 17:19:07 +0800

    Make UBigInt class parse and print big integer

--- src/UBigInt.cc
+++ src/UBigInt.cc
@@ -5,6 +5,16 @@ UBigInt::UBigInt() {
 }
 
 UBigInt::UBigInt(std::string strval) {
+  // strip off leading zeros from the input string
+  strval.erase(0, strval.find_first_not_of('0'));
+  // strip off leading spaces from the input string
+  strval.erase(0, strval.find_first_not_of(' '));
+
+  if (strval.size() <= 0) {
+    m_value.push_back(0);
+    return;
+  }
+
   size_t decimalLength = strval.length();
   uint8_t *decimalArr = new uint8_t[decimalLength](); 
   for (int i = 0; i < decimalLength; i++) {
@@ -25,15 +35,25 @@ UBigInt::UBigInt(std::string strval) {
     if (!decimalArr[decimalNonZeroIdx]) {
       decimalNonZeroIdx++;
     }
+
+    if (bitIndex == 64) {
+      uint64_t value = 0;
+      for (int i = bitIndex - 1; i >= 0; --i) {
+        value += bitArr[i] * 1;
+        if (i != 0) { value <<= 1; }
+      }
+      m_value.push_back(value);
+      bitIndex = 0;
+      bitArr = new uint8_t[64]();
+    }
   }
-  
+
   uint64_t value = 0;
   for (int i = bitIndex - 1; i >= 0; --i) {
     value += bitArr[i] * 1;
     if (i != 0) { value <<= 1; }
   }
-  
-  m_value.push_back(value);
+  if (value > 0) { m_value.push_back(value); }
 }
 
 std::size_t UBigInt::length() {
@@ -41,7 +61,49 @@ std::size_t UBigInt::length() {
 }
 
 std::string UBigInt::toString() {
-  return "0";
+
+  std::vector<uint8_t> decimalArr;
+  decimalArr.push_back(0);
+
+  for (int i = m_value.size() - 1; i >= 0; --i) {
+    int maxBitIdx = 63;
+    // if (i == m_value.size() - 1) {
+    //   maxBitIdx = __builtin_ctzll(m_value[i]) + 1;
+    // }
+    for (int j = maxBitIdx; j >= 0; --j) {
+      uint8_t carry = 0;
+      for (int m = 0; m < decimalArr.size(); ++m) {
+        decimalArr[m] *= 2;
+        decimalArr[m] += carry;
+        carry = 0;
+        if (decimalArr[m] > 9) {
+          decimalArr[m] -= 10;
+          carry = 1;
+        }
+      }
+
+      if (carry == 1) {
+        decimalArr.push_back(1);
+        carry = 0;
+      }
+
+      uint64_t bMask = 1;
+      for (int k = 0; k < j; ++k) {
+        bMask <<= 1;
+      }
+      if ((m_value[i] & bMask) != 0) {
+        decimalArr[0] += 1;
+      }
+    }
+  }
+
+  std::string printValue;
+  for (int i = decimalArr.size() - 1; i >= 0; --i) {
+    printValue.push_back(decimalArr[i] + '0');
+  }
+
+  // std::cout << printValue << "\n";
+  return printValue;
 }
 
 std::uint64_t UBigInt::toUInt64() {
--- tests/UBigIntTests.cc
+++ tests/UBigIntTests.cc
@@ -9,8 +9,36 @@ TEST(UBigIntTests, EmptyConstructorCreatesZero) {
 }
 
 TEST(UBigIntTests, CreatesBigUnsignedIntegerWithStringRepresentation) {
-  UBigInt bigInt("123");
+  UBigInt bigInt("0");
   ASSERT_EQ(bigInt.length(), 1);
-  ASSERT_EQ(bigInt.toUInt64(), 123);
+  ASSERT_EQ(bigInt.toUInt64(), 0ULL);
+  ASSERT_EQ(bigInt.toString(), "0");
+
+  bigInt = UBigInt("10");
+  ASSERT_EQ(bigInt.length(), 1);
+  ASSERT_EQ(bigInt.toUInt64(), 10ULL);
+  ASSERT_EQ(bigInt.toString(), "10");
+
+  bigInt = UBigInt("275");
+  ASSERT_EQ(bigInt.length(), 1);
+  ASSERT_EQ(bigInt.toUInt64(), 275ULL);
+  ASSERT_EQ(bigInt.toString(), "275");
+  
+  bigInt = UBigInt("12345678901234567890");
+  ASSERT_EQ(bigInt.length(), 1);
+  ASSERT_EQ(bigInt.toUInt64(), 12345678901234567890ULL);
+  ASSERT_EQ(bigInt.toString(), "12345678901234567890");
+
+  bigInt = UBigInt("123456789012345678901");
+  ASSERT_EQ(bigInt.length(), 2);
+  ASSERT_EQ(bigInt.toString(), "123456789012345678901");
+
+  bigInt = UBigInt("1234567890123456789012345678901234567890");
+  ASSERT_EQ(bigInt.length(), 3);
+  ASSERT_EQ(bigInt.toString(), "1234567890123456789012345678901234567890");
+
+  bigInt = UBigInt("123456789012345678901234567890123456789012345678901234567890");
+  ASSERT_EQ(bigInt.length(), 4);
+  ASSERT_EQ(bigInt.toString(), "123456789012345678901234567890123456789012345678901234567890");
 }
 
