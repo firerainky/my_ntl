08248ad7eea5948753603c9288d13bc579443378
zhengkanyan <zhengkanyan@zhejianglab.com>
2023-04-23 17:31:55 +0800

    Reformat all source files with default formating

--- src/UBigInt.cc
+++ src/UBigInt.cc
@@ -1,196 +1,254 @@
 #include "UBigInt.h"
 
-UBigInt::UBigInt() { m_value.push_back(0); }
-
-UBigInt::UBigInt(uint64_t val) { m_value.push_back(val); }
-
-UBigInt::UBigInt(std::vector<std::uint64_t> vals) { m_value = vals; }
-
-UBigInt::UBigInt(std::string strval) {
-  // strip off leading zeros from the input string
-  strval.erase(0, strval.find_first_not_of('0'));
-  // strip off leading spaces from the input string
-  strval.erase(0, strval.find_first_not_of(' '));
-
-  if (strval.size() <= 0) {
+UBigInt::UBigInt()
+{
     m_value.push_back(0);
-    return;
-  }
+}
 
-  size_t decimalLength = strval.length();
-  uint8_t *decimalArr = new uint8_t[decimalLength]();
-  for (int i = 0; i < decimalLength; i++) {
-    decimalArr[i] = strval[i] - '0';
-  }
-  uint8_t *bitArr = new uint8_t[64]();
-  uint8_t bitIndex = 0;
-  uint8_t decimalNonZeroIdx = 0;
-  while (decimalNonZeroIdx < decimalLength) {
-    bitArr[bitIndex++] = decimalArr[decimalLength - 1] % 2;
+UBigInt::UBigInt(uint64_t val)
+{
+    m_value.push_back(val);
+}
 
-    for (int i = decimalNonZeroIdx; i < decimalLength - 1; i++) {
-      decimalArr[i + 1] += (decimalArr[i] % 2) * 10;
-      decimalArr[i] >>= 1;
+UBigInt::UBigInt(std::vector<std::uint64_t> vals)
+{
+    m_value = vals;
+}
+
+UBigInt::UBigInt(std::string strval)
+{
+    // strip off leading zeros from the input string
+    strval.erase(0, strval.find_first_not_of('0'));
+    // strip off leading spaces from the input string
+    strval.erase(0, strval.find_first_not_of(' '));
+
+    if (strval.size() <= 0)
+    {
+        m_value.push_back(0);
+        return;
     }
-    decimalArr[decimalLength - 1] >>= 1;
 
-    if (!decimalArr[decimalNonZeroIdx]) {
-      decimalNonZeroIdx++;
+    size_t decimalLength = strval.length();
+    uint8_t *decimalArr = new uint8_t[decimalLength]();
+    for (int i = 0; i < decimalLength; i++)
+    {
+        decimalArr[i] = strval[i] - '0';
+    }
+    uint8_t *bitArr = new uint8_t[64]();
+    uint8_t bitIndex = 0;
+    uint8_t decimalNonZeroIdx = 0;
+    while (decimalNonZeroIdx < decimalLength)
+    {
+        bitArr[bitIndex++] = decimalArr[decimalLength - 1] % 2;
+
+        for (int i = decimalNonZeroIdx; i < decimalLength - 1; i++)
+        {
+            decimalArr[i + 1] += (decimalArr[i] % 2) * 10;
+            decimalArr[i] >>= 1;
+        }
+        decimalArr[decimalLength - 1] >>= 1;
+
+        if (!decimalArr[decimalNonZeroIdx])
+        {
+            decimalNonZeroIdx++;
+        }
+
+        if (bitIndex == 64)
+        {
+            uint64_t value = 0;
+            for (int i = bitIndex - 1; i >= 0; --i)
+            {
+                value += bitArr[i] * 1;
+                if (i != 0)
+                {
+                    value <<= 1;
+                }
+            }
+            m_value.push_back(value);
+            bitIndex = 0;
+            bitArr = new uint8_t[64]();
+        }
     }
 
-    if (bitIndex == 64) {
-      uint64_t value = 0;
-      for (int i = bitIndex - 1; i >= 0; --i) {
+    uint64_t value = 0;
+    for (int i = bitIndex - 1; i >= 0; --i)
+    {
         value += bitArr[i] * 1;
-        if (i != 0) {
-          value <<= 1;
+        if (i != 0)
+        {
+            value <<= 1;
         }
-      }
-      m_value.push_back(value);
-      bitIndex = 0;
-      bitArr = new uint8_t[64]();
     }
-  }
-
-  uint64_t value = 0;
-  for (int i = bitIndex - 1; i >= 0; --i) {
-    value += bitArr[i] * 1;
-    if (i != 0) {
-      value <<= 1;
+    if (value > 0)
+    {
+        m_value.push_back(value);
     }
-  }
-  if (value > 0) {
-    m_value.push_back(value);
-  }
 }
 
-UBigInt UBigInt::add(UBigInt num) const {
-  std::vector<uint64_t> resultVectors;
+UBigInt UBigInt::add(UBigInt num) const
+{
+    std::vector<uint64_t> resultVectors;
 
-  uint8_t carry = 0;
-  uint64_t currentLimb;
-  int i = 0;
-  while (i < length() && i < num.length()) {
-    carry = addWithCarry(m_value[i], num.m_value[i], carry, &currentLimb);
-    resultVectors.push_back(currentLimb);
-    ++i;
-  }
-  while (i < length()) {
-    carry = addWithCarry(m_value[i], 0, carry, &currentLimb);
-    resultVectors.push_back(currentLimb);
-    ++i;
-  }
-  while (i < num.length()) {
-    carry = addWithCarry(0, num.m_value[i], carry, &currentLimb);
-    resultVectors.push_back(currentLimb);
-    ++i;
-  }
-
-  return UBigInt(resultVectors);
-}
-
-UBigInt UBigInt::sub(UBigInt num) const {
-  std::vector<uint64_t> resultVectors;
-
-  uint8_t borrow = 0;
-  uint64_t currentLimb;
-
-  for (int i = 0; i < length(); ++i) {
-    if (i >= num.length()) {
-      borrow = subWithBorrow(m_value[i], 0, borrow, &currentLimb);
-      resultVectors.push_back(currentLimb);
-    } else {
-      borrow = subWithBorrow(m_value[i], num.m_value[i], borrow, &currentLimb);
-      resultVectors.push_back(currentLimb);
+    uint8_t carry = 0;
+    uint64_t currentLimb;
+    int i = 0;
+    while (i < length() && i < num.length())
+    {
+        carry = addWithCarry(m_value[i], num.m_value[i], carry, &currentLimb);
+        resultVectors.push_back(currentLimb);
+        ++i;
     }
-  }
+    while (i < length())
+    {
+        carry = addWithCarry(m_value[i], 0, carry, &currentLimb);
+        resultVectors.push_back(currentLimb);
+        ++i;
+    }
+    while (i < num.length())
+    {
+        carry = addWithCarry(0, num.m_value[i], carry, &currentLimb);
+        resultVectors.push_back(currentLimb);
+        ++i;
+    }
 
-  return UBigInt(resultVectors);
+    return UBigInt(resultVectors);
 }
 
-UBigInt UBigInt::multiply(UBigInt num) const {
-  std::vector<uint64_t> value;
-  for (int i = 0; i < length(); ++i) {
-    for (int j = 0; j < num.length(); ++j) {
-      uint64_t temp_result[2];
-      multiplyWithKaratsuba(m_value[i], num.m_value[j], temp_result);
-      uint8_t carry = 0;
-      uint64_t sum;
-      if (i + j + 1 > value.size()) {
-        value.push_back(temp_result[0]);
-      } else {
-        carry = addWithCarry(temp_result[0], value[i + j], carry, &sum);
-        value[i + j] = sum;
-        temp_result[1] += carry;
-        carry = 0;
-      }
+UBigInt UBigInt::sub(UBigInt num) const
+{
+    std::vector<uint64_t> resultVectors;
 
-      if (i + j + 2 > value.size()) {
-        value.push_back(temp_result[1]);
-      } else {
-        carry = addWithCarry(temp_result[1], value[i + j + 1], carry, &sum);
-        value[i + j + 1] = sum;
-        uint8_t currentIdx = i + j + 2;
-        while (carry) {
-          if (currentIdx > value.size()) {
-            value.push_back(carry);
-          } else {
-            carry = addWithCarry(0, value[currentIdx], carry, &sum);
-            value[currentIdx] = sum;
-          }
+    uint8_t borrow = 0;
+    uint64_t currentLimb;
+
+    for (int i = 0; i < length(); ++i)
+    {
+        if (i >= num.length())
+        {
+            borrow = subWithBorrow(m_value[i], 0, borrow, &currentLimb);
+            resultVectors.push_back(currentLimb);
         }
-      }
-    }
-  }
-
-  return UBigInt(value);
-}
-
-std::size_t UBigInt::length() const { return m_value.size(); }
-
-std::string UBigInt::toString() {
-  std::vector<uint8_t> decimalArr;
-  decimalArr.push_back(0);
-
-  for (int i = m_value.size() - 1; i >= 0; --i) {
-    int maxBitIdx = 63;
-    // if (i == m_value.size() - 1) {
-    //   maxBitIdx = __builtin_ctzll(m_value[i]) + 1;
-    // }
-    for (int j = maxBitIdx; j >= 0; --j) {
-      uint8_t carry = 0;
-      for (int m = 0; m < decimalArr.size(); ++m) {
-        decimalArr[m] *= 2;
-        decimalArr[m] += carry;
-        carry = 0;
-        if (decimalArr[m] > 9) {
-          decimalArr[m] -= 10;
-          carry = 1;
+        else
+        {
+            borrow = subWithBorrow(m_value[i], num.m_value[i], borrow, &currentLimb);
+            resultVectors.push_back(currentLimb);
         }
-      }
-
-      if (carry == 1) {
-        decimalArr.push_back(1);
-        carry = 0;
-      }
-
-      uint64_t bMask = 1;
-      for (int k = 0; k < j; ++k) {
-        bMask <<= 1;
-      }
-      if ((m_value[i] & bMask) != 0) {
-        decimalArr[0] += 1;
-      }
     }
-  }
 
-  std::string printValue;
-  for (int i = decimalArr.size() - 1; i >= 0; --i) {
-    printValue.push_back(decimalArr[i] + '0');
-  }
-
-  // std::cout << printValue << "\n";
-  return printValue;
+    return UBigInt(resultVectors);
 }
 
-std::uint64_t UBigInt::toUInt64() { return m_value[0]; }
+UBigInt UBigInt::multiply(UBigInt num) const
+{
+    std::vector<uint64_t> value;
+    for (int i = 0; i < length(); ++i)
+    {
+        for (int j = 0; j < num.length(); ++j)
+        {
+            uint64_t temp_result[2];
+            multiplyWithKaratsuba(m_value[i], num.m_value[j], temp_result);
+            uint8_t carry = 0;
+            uint64_t sum;
+            if (i + j + 1 > value.size())
+            {
+                value.push_back(temp_result[0]);
+            }
+            else
+            {
+                carry = addWithCarry(temp_result[0], value[i + j], carry, &sum);
+                value[i + j] = sum;
+                temp_result[1] += carry;
+                carry = 0;
+            }
+
+            if (i + j + 2 > value.size())
+            {
+                value.push_back(temp_result[1]);
+            }
+            else
+            {
+                carry = addWithCarry(temp_result[1], value[i + j + 1], carry, &sum);
+                value[i + j + 1] = sum;
+                uint8_t currentIdx = i + j + 2;
+                while (carry)
+                {
+                    if (currentIdx > value.size())
+                    {
+                        value.push_back(carry);
+                    }
+                    else
+                    {
+                        carry = addWithCarry(0, value[currentIdx], carry, &sum);
+                        value[currentIdx] = sum;
+                    }
+                }
+            }
+        }
+    }
+
+    return UBigInt(value);
+}
+
+std::size_t UBigInt::length() const
+{
+    return m_value.size();
+}
+
+std::string UBigInt::toString()
+{
+    std::vector<uint8_t> decimalArr;
+    decimalArr.push_back(0);
+
+    for (int i = m_value.size() - 1; i >= 0; --i)
+    {
+        int maxBitIdx = 63;
+        // if (i == m_value.size() - 1) {
+        //   maxBitIdx = __builtin_ctzll(m_value[i]) + 1;
+        // }
+        for (int j = maxBitIdx; j >= 0; --j)
+        {
+            uint8_t carry = 0;
+            for (int m = 0; m < decimalArr.size(); ++m)
+            {
+                decimalArr[m] *= 2;
+                decimalArr[m] += carry;
+                carry = 0;
+                if (decimalArr[m] > 9)
+                {
+                    decimalArr[m] -= 10;
+                    carry = 1;
+                }
+            }
+
+            if (carry == 1)
+            {
+                decimalArr.push_back(1);
+                carry = 0;
+            }
+
+            uint64_t bMask = 1;
+            for (int k = 0; k < j; ++k)
+            {
+                bMask <<= 1;
+            }
+            if ((m_value[i] & bMask) != 0)
+            {
+                decimalArr[0] += 1;
+            }
+        }
+    }
+
+    std::string printValue;
+    for (int i = decimalArr.size() - 1; i >= 0; --i)
+    {
+        printValue.push_back(decimalArr[i] + '0');
+    }
+
+    // std::cout << printValue << "\n";
+    return printValue;
+}
+
+std::uint64_t UBigInt::toUInt64()
+{
+    return m_value[0];
+}
--- src/UBigInt.h
+++ src/UBigInt.h
@@ -1,37 +1,38 @@
 ﻿#include <iostream>
 #include <vector>
 
-class UBigInt {
- public:
-  UBigInt();
-  UBigInt(std::uint64_t val);
-  UBigInt(std::vector<std::uint64_t> vals);
-  UBigInt(std::string strval);
+class UBigInt
+{
+public:
+    UBigInt();
+    UBigInt(std::uint64_t val);
+    UBigInt(std::vector<std::uint64_t> vals);
+    UBigInt(std::string strval);
 
-  UBigInt add(UBigInt num) const;
-  UBigInt sub(UBigInt num) const;
-  UBigInt multiply(UBigInt num) const;
+    UBigInt add(UBigInt num) const;
+    UBigInt sub(UBigInt num) const;
+    UBigInt multiply(UBigInt num) const;
 
-  std::size_t length() const;
-  std::string toString();
-  std::uint64_t toUInt64();
+    std::size_t length() const;
+    std::string toString();
+    std::uint64_t toUInt64();
 
- private:
-  std::vector<uint64_t> m_value;
+private:
+    std::vector<uint64_t> m_value;
 };
 
-inline uint8_t addWithCarry(uint64_t operand1, uint64_t operand2, uint8_t carry,
-                            uint64_t *result) {
-  operand1 += operand2;
-  *result = operand1 + carry;
-  return (operand1 < operand2) || (~operand1 < carry);
+inline uint8_t addWithCarry(uint64_t operand1, uint64_t operand2, uint8_t carry, uint64_t *result)
+{
+    operand1 += operand2;
+    *result = operand1 + carry;
+    return (operand1 < operand2) || (~operand1 < carry);
 }
 
-inline uint8_t subWithBorrow(uint64_t operand1, uint64_t operand2,
-                             uint8_t borrow, uint64_t *result) {
-  auto diff = operand1 - operand2;
-  *result = diff - (borrow != 0);
-  return (diff > operand1) || (diff < borrow);
+inline uint8_t subWithBorrow(uint64_t operand1, uint64_t operand2, uint8_t borrow, uint64_t *result)
+{
+    auto diff = operand1 - operand2;
+    *result = diff - (borrow != 0);
+    return (diff > operand1) || (diff < borrow);
 }
 
 /**
@@ -40,20 +41,18 @@ inline uint8_t subWithBorrow(uint64_t operand1, uint64_t operand2,
  * (a * 2^n + b) * (c * 2^n + d) = ac*2^2n + (ad + bc)*2^n + bd
  * 这里 n = 32，下面的计算时由于进位的原因，做了多次平移。
  */
-inline void multiplyWithKaratsuba(uint64_t operand1, uint64_t operand2,
-                                  uint64_t *result128) {
-  uint64_t a = operand1 >> 32;
-  uint64_t b = operand1 & 0x00000000FFFFFFFF;
-  uint64_t c = operand2 >> 32;
-  uint64_t d = operand2 & 0x00000000FFFFFFFF;
+inline void multiplyWithKaratsuba(uint64_t operand1, uint64_t operand2, uint64_t *result128)
+{
+    uint64_t a = operand1 >> 32;
+    uint64_t b = operand1 & 0x00000000FFFFFFFF;
+    uint64_t c = operand2 >> 32;
+    uint64_t d = operand2 & 0x00000000FFFFFFFF;
 
-  uint64_t right = b * d;
-  uint64_t middle;
-  uint64_t left =
-      a * c +
-      (static_cast<uint64_t>(addWithCarry(a * d, b * c, 0, &middle)) << 32);
-  uint64_t temp_sum = (right >> 32) + (middle & 0x000000000FFFFFFFF);
+    uint64_t right = b * d;
+    uint64_t middle;
+    uint64_t left = a * c + (static_cast<uint64_t>(addWithCarry(a * d, b * c, 0, &middle)) << 32);
+    uint64_t temp_sum = (right >> 32) + (middle & 0x000000000FFFFFFFF);
 
-  result128[1] = left + (middle >> 32) + (temp_sum >> 32);
-  result128[0] = (temp_sum << 32) | (right & 0x00000000FFFFFFFF);
+    result128[1] = left + (middle >> 32) + (temp_sum >> 32);
+    result128[0] = (temp_sum << 32) | (right & 0x00000000FFFFFFFF);
 }
