62a9fee8f0f9b7d18b4218de4025c64c790e1597
zhengkanyan <zhengkanyan@zhejianglab.com>
2023-05-22 18:30:56 +0800

    Add for_each test

--- tests/CeilIntTests.cc
+++ tests/CeilIntTests.cc
@@ -4,7 +4,16 @@
 //     uint32_t mask =
 // }
 
-TEST(CeilIntTests, Uint64Constructor) {
-    auto v = uint64_t(123);
-    ASSERT_EQ(v, 123);
+void println(auto const& v)
+{
+    for (auto count{v.size()}; auto const& e : v)
+        std::cout << e << (--count ? ", " : "\n");
+}
+
+TEST(CeilIntTests, ForEachTest) {
+    std::vector<int> vi {1, 2, 3, 4, 5};
+    println(vi);
+
+    std::for_each_n(vi.begin(), 3, [](auto& n) { n *= 2; });
+    println(vi);
 }
